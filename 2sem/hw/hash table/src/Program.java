94da6bc87705b4b0feeb52d8c48876e321c13afb
SvetlanaShipovalova <90263099+SvetlanaShipovalova@users.noreply.github.com>
2022-06-09 20:43:02 +0300

    .

---
+++ 2sem/hw/hash table/src/Program.java
@@ -0,0 +1,506 @@
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Offset_hash_1");
+        HashTable hashTable = new OffsetHashTable(13);
+
+        hashTable.insert(35);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(47);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(84);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(39);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(89);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(18);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(2);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(77);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(13);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(73);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(70);
+        hashTable.print();
+        System.out.println();
+
+        hashTable.insert(32);
+        hashTable.print();
+        System.out.println();
+
+System.out.println("Offset_hash_2");
+
+        HashTable hashTable1 = new OffsetHashTable(19);
+
+        hashTable1.insert(95);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(26);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(34);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(5);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(66);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(25);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(70);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(84);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(61);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(25);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(83);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(6);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(16);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(63);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.insert(23);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.remove(23);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.remove(70);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.remove(6);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.remove(34);
+        hashTable1.print();
+        System.out.println();
+
+        hashTable1.remove(63);
+        hashTable1.print();
+        System.out.println();
+
+        System.out.println("Offset_hash_3");
+
+        HashTable hashTable2 = new OffsetHashTable(19);
+
+        hashTable2.insert(88);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(72);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(53);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(93);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(27);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(89);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(4);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(69);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(65);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(76);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(88);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(91);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(93);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(89);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(88);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(34);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(84);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(71);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(53);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(91);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(45);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(94);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.insert(70);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(69);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(27);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(70);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(71);
+        hashTable2.print();
+        System.out.println();
+
+        hashTable2.remove(45);
+        hashTable2.print();
+        System.out.println();
+
+        System.out.println("List_hash_1");
+        HashTable listHashTable = new ListHashTable(13);
+
+        listHashTable.insert(74);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(92);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(32);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(13);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(18);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(78);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(74);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(96);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(29);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(55);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(97);
+        listHashTable.print();
+        System.out.println();
+
+        listHashTable.insert(70);
+        listHashTable.print();
+        System.out.println();
+
+        System.out.println("List_hash_2");
+
+        HashTable listHashTable1 = new ListHashTable(19);
+
+        listHashTable1.insert(65);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(13);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(85);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(39);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(64);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(34);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(47);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(61);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(69);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(36);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(53);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(88);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(37);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(7);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.insert(92);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.remove(85);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.remove(92);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.remove(37);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.remove(88);
+        listHashTable1.print();
+        System.out.println();
+
+        listHashTable1.remove(64);
+        listHashTable1.print();
+        System.out.println();
+
+        System.out.println("List_hash_3");
+
+        HashTable listHashTable2 = new ListHashTable(19);
+
+
+        listHashTable2.insert(94);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(25);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(21);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(97);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(23);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(17);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(19);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(12);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(35);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(52);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(93);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(23);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(94);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(17);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(35);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(87);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(76);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(10);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(87);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(97);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(55);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(48);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.insert(40);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(12);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(40);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(52);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(21);
+        listHashTable2.print();
+        System.out.println();
+
+        listHashTable2.remove(19);
+        listHashTable2.print();
+        System.out.println();
+    }
+}
